b7df906d84210c6aa7934d477f621acc3c652033
nicklez5 <jackson2k11@gmail.com>
2025-08-11 05:31:31 -0700

    Fix Identity boolean column types for Postgres

--- MyApi/obj/Debug/net9.0/MyApi.AssemblyInfo.cs
+++ MyApi/obj/Debug/net9.0/MyApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2abcd021e00fa95f2abb3ca6a191a7202a02c367")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8abeda012a0a9b69312c235d59e8e259e9517322")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
