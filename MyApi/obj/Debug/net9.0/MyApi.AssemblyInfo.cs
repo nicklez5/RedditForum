120593b43361bc43a35691de33e1d0e4df78ae8f
nicklez5 <jackson2k11@gmail.com>
2025-09-20 08:28:46 -0700

    update

--- MyApi/obj/Debug/net9.0/MyApi.AssemblyInfo.cs
+++ MyApi/obj/Debug/net9.0/MyApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be8dda27698afca8b6d06531286e5d2323d48aac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+410e645cda20d3ed00317de74f6ec254f880ce88")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
